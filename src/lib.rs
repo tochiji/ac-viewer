669b4942c771aa9740e3dd2a92850f566dfd4fa8
tochiji <7910558+tochiji@users.noreply.github.com>
2022-01-22 19:31:47 +0900

    hande no submission

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,11 @@ pub async fn run(contest_id: String, problem_id: String) -> Result<(), Box<dyn s
     let mut problem = Problem::new(contest_id, problem_id);
     problem.get_next_list().await?;
 
+    if problem.list.len() == 0 {
+        println!("No Submission Found");
+        return Ok(());
+    }
+
     loop {
         problem.submission_select().await?;
     }
